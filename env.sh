4f41ae2ad5f962534b13af747486e27e1f41f980
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2014-12-08 20:53:27 +0200

    Deleted G from VM size.

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export OS_IMAGE_DIR="/home/asayankin/os-images"
 export OS_IMAGE_FORMAT=qcow2
-export OS_IMAGE_SIZE=30G
+export OS_IMAGE_SIZE=30 #GB
 export OS_IMAGE_ROOT_PASSWD=Al3ks3y7!
 export OS_TYPE='Ubuntu14.04'
 export MAPR_VERSION='4.0.1'
