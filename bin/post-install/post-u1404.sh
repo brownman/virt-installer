49f80a835bf7f88921df869232b506b379fd7be2
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2015-02-11 18:13:17 +0200

    Added Kiev timezone setup for Ubuntu.

--- bin/post-install/post-u1404.sh
+++ bin/post-install/post-u1404.sh
@@ -14,6 +14,10 @@ cat >> /etc/hosts << EOF
 $IP_ETH0  $OS_IMAGE_HOST_NAME  $OS_IMAGE_HOST_ALIAS
 EOF
 
+# Setup timezone
+echo "Europe/Kiev" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
 # Setup storage
 losetup /dev/loop0 /mapr-disks/disk0
 
